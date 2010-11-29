1949ee3fd911413a46754e7c22b2584d5ea128e6
unknown <Alec@.thecrimson.net>
2010-11-29 16:23:50 -0500

    created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G8keep::Application.config.secret_token = 'd260bcddb05dad6837d919d643553d48f676ddb5e7f3f638a6f6fb872f174989fe514a2e55d33d5d773f541ba391b55e04a90e92e862998bead4ab689a97b681'
