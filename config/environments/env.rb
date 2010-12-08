2a872e173b65107ced493c815f320e27e4490275
Alec Guzov <avguzov@gmail.com>
2010-12-08 10:54:21 -0500

    Tried fixing email

--- config/environments/env.rb
+++ config/environments/env.rb
@@ -1,9 +1,1 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'baci.lindsaar.net',
-  :user_name            => '<username>',
-  :password             => '<password>',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
+ActionMailer::Base.delivery_method = :sendmail
